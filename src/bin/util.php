6b3e4e550aac951f8f00591800e9210a0fb0b80c
TurtleEngr <turtle.engr@gmail.com>
2025-04-20 15:05:27 -0700

    Updated

--- src/bin/util.php
+++ src/bin/util.php
@@ -189,7 +189,7 @@ function uPackFile($pDocFile, $pFileList) {
     foreach ($tList as $tFile) {
         shell_exec("/bin/bash -c 'cd $cgDirTmp; tidy $cTidyOpt $tFile.xml'");
         # Remove newlines between tags, to remove any spaces in the text
-        shell_exec("/bin/bash -c \"cd $cgDirTmp; sed -i \'s|\n*| |g\' $tFile.xml\"");
+        shell_exec("/bin/bash -c \"cd $cgDirTmp; sed -i 's|\n| |g' $tFile.xml\"");
         shell_exec("/bin/bash -c 'cd $cgDirTmp; zip ../$pDocFile $tFile.xml'");
     }
 
